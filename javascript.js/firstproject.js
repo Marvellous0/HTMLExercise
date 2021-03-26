1428711049ff19bdcdf9277e2b89330c950d9a14
Marvellous <adeoyemarvellous7@gmail.com>
2021-03-26 13:04:18 +0100

    initial commit

---
+++ javascript.js/firstproject.js
@@ -0,0 +1,13 @@
+// var name = "ade";
+// console.log(name);
+// let age = 24;
+// console.log(age);
+var y = 5 !== '5';
+console.log(y);
+// function myfunction() {
+    
+        // console.log(name);
+        // console.log(age);
+     
+//}        myfunction();
+    
